16dcdf2989e58690059394ef872ba61d1ecae053
abh1rajj <abhirajtauhid@gmail.com>
2025-07-17 13:07:12 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperrations.js
@@ -0,0 +1,28 @@
+let score = false
+
+// console.log (typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc"=> NaN (not in number)
+// true => 1; false => 0
+
+let isLoggedIn = "abhiraj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// ""=> false
+// "abhiraj" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
