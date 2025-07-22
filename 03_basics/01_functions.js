553bf7c1a01cba6f130850d74d021dac976a7c2a
abh1rajj <abhirajtauhid@gmail.com>
2025-07-22 22:23:20 +0000

    Functions with objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,13 +27,45 @@ const result = addTwoNumbers(3,4)
 
 // console.log("Result: ", result );
 
-function loginUserMessage(username = "sam" ){
-    if(!username){
-        console.log("Please enter a username");
-        return 
-    }
-    return `${username} just logged in`
-}
+// function loginUserMessage(username = "sam" ){
+//     if(!username){
+//         console.log("Please enter a username");
+//         return 
+//     }
+//     return `${username} just logged in`
+// }
 
 // console.log(loginUserMessage("hitesh"));
-console.log(loginUserMessage("abhiraj"));
+// console.log(loginUserMessage("abhiraj"));
+
+
+function calculateCartPrice(val1, val2,...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username : "abhiraj" , 
+    price : 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+
+}
+
+// handleObject(user)
+handleObject({
+    username : "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 800, 100, 600]));
