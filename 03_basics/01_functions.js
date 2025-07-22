e44b462472ff40b261e151ae306fc57b482c2a9d
abh1rajj <abhirajtauhid@gmail.com>
2025-07-22 21:00:25 +0000

    functions1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,39 @@
+function sayMyName () {
+console.log("A");
+console.log("B");
+console.log("H");
+console.log("I");
+console.log("R");
+console.log("A");
+console.log("J");
+}
+
+// sayMyName()
+
+// function addTwoNumbers (number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers (number1, number2){
+
+    // let result = number1 + number2
+    // return result
+ return number1 + number2
+}
+
+
+const result = addTwoNumbers(3,4)
+
+// console.log("Result: ", result );
+
+function loginUserMessage(username = "sam" ){
+    if(!username){
+        console.log("Please enter a username");
+        return 
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"));
+console.log(loginUserMessage("abhiraj"));
