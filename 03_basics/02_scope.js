9331d6efccb539a713584e26eeadca582ee1d39b
abh1rajj <abhirajtauhid@gmail.com>
2025-07-24 19:18:06 +0000

    Scope level and hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -8,6 +8,49 @@ if (true) {
     
 }
 
-console.log(a);
+
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one() {
+    const username = "abhiraj"
+    
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two() 
+}
+
+// one()
+
+
+if (true) {
+    const username = "abhiraj"
+    if (username === "abhiraj") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++ interesting ++++++++++++
+
+
+addOne(5)
+function addOne(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
